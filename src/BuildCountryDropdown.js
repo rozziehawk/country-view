c6e4dcd69ce2da728d2c81c35f9011c5c788c09e
Karl Haakonsen <cityhawk@pobox.com>
2024-05-08 13:18:40 -0400

    more dropdown functionality

--- src/BuildCountryDropdown.js
+++ src/BuildCountryDropdown.js
@@ -4,13 +4,34 @@ function buildCountryDropDown(countries)
     let jsxCountries = [];
     let value;
 
-    const arrItems = [<option value="Barbados">Barbados</option>, <option value="Brazil">Brazil</option>, 
-  <option value="Canada">Canada</option>]
+    countries.sort();
 
     for (let country of countries) 
-    {   
-         //jsxCountries += '<option value="' + country + '">' + country + '</option>';
-         jsxCountries.push(<option value={country}>{country}</option>)
+    {   let value;
+         let iComma = country.indexOf(",");
+         let length = country.length;
+         //first handle special cases:
+         if (country.substring(0, 12) == "Saint Martin")
+         {
+          value = "Saint Martin";
+         }
+         else if (country.substring(0,7) == "Bonaire")
+         {
+            value = country;
+         }
+         else if (country.substring(0,5) == "Congo")
+         {
+            value = "Democratic Republic of the Congo";
+         }
+         else if (iComma > 0)
+         {
+            value = country.substring(iComma + 2, length) + " " + country.substring(0, iComma);
+         }
+         else
+        {
+            value = country;
+        }
+        jsxCountries.push(<option value={value}>{country}</option>)
     }
     
     //return jsxCountries;
