ed21e915ffd8a28a3eeabcc8decbd7e2725bea1d
Karl Haakonsen <cityhawk@pobox.com>
2024-06-03 14:24:21 -0400

    Add appropriate links to Navbar dropdown

--- src/Header.js
+++ src/Header.js
@@ -11,10 +11,13 @@ const Header = () => {
             Tools Used
           </button>
           <ul className="dropdown-menu" aria-labelledby="tools-used"s>
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" href="https://nodejs.org/en" target="_new">Node JS</a></li>
+            <li><a className="dropdown-item" href="https://react.dev/learn" target="_new">React JS</a></li>
+            <li><a className="dropdown-item" href="https://getbootstrap.com/docs/5.3/getting-started/introduction/" target="_new">Bootstrap</a></li>
+            <li><a className="dropdown-item" href="https://restcountries.com/" target="_new">REST Countries</a></li>
+            <li><a className="dropdown-item" href="https://axios-http.com/docs/intro" target="_new">AXIOS</a></li>
             <li><a classNaName="dropdown-divider"></a></li>
-            <li><a className="dropdown-item" href="#">Separated link</a></li>
+            <li><a className="dropdown-item" href="http://www.github.com" target="_new">GitHub</a></li>
           </ul>
         </div>
 
